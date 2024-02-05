88039b28e91c67d6d61481e0de22ca6352dd82da
m.kuzhagulov <m.kuzhagulov@gmail.com>
2024-02-05 14:22:27 +0700

    added README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# fadi-task
+
+## API
+ - calculate statistics from given input data about political speeches:
+```
+GET localhost:9000/evaluation?url=url1&url=url2
+```
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -10,23 +10,12 @@ import play.api.libs.json.Json
 
 import scala.concurrent.ExecutionContext
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(politicalSpeechesService: PoliticalSpeechesService,
                                val controllerComponents: ControllerComponents)
                               (implicit ec: ExecutionContext)
   extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
